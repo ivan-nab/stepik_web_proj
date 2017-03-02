a8f38e4e1568bb2abd0166df8d26e47bbfad95ac
иван набойченко < >
2017-03-02 23:54:46 +0300

    added gunicorn.conf

---
+++ hello.py
@@ -0,0 +1,14 @@
+
+from urlparse import parse_qs
+
+def wsgi_application(environ, start_response):    
+    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+    body = []
+    for key, value in query.items():
+        for item in value:
+            body.append(key + "=" + item + "\n")
+    status = '200 OK'     
+    headers = [('Content-Type', 'text/plain')]
+    body = 'Hello, world!'
+    start_response(status, headers )     
+    return [ body ] 
