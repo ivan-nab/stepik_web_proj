8824613cee44a68f3f89bd1e06b3f423d13eb530
иван набойченко < >
2017-03-03 00:18:47 +0300

    refactoring

--- hello.py
+++ hello.py
@@ -1,13 +1,7 @@
 
-from urlparse import parse_qs
 
-def wsgi_application(environ, start_response):    
-    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-    body = []
-    for key, value in query.items():
-        for item in value:
-            text = str(key) + "=" + str(item) + "\n"
-            body.append(text)
+def wsgi_application(environ, start_response):        
+    body = "\n".join(environ['QUERY_STRING'].split('&'))
     status = '200 OK'     
     headers = [('Content-Type', 'text/plain')]
     start_response(status, headers )     
