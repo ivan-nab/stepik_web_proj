64342039208c7092bc7a451cff568b43a1688dd6
иван набойченко < >
2017-03-11 16:19:43 +0300

    settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -80,12 +80,6 @@ DATABASES = {
         'USER': 'qa',
         'PASSWORD': 'qapass',
     }
-    ''' 
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    '''
 }
 
 
