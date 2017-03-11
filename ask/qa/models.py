ff211457b917fc107e703fe548c37592bd375806
иван набойченко < >
2017-03-11 16:33:24 +0300

    models.py - fixed tabs

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User  
 # Create your models here.
-class QuestionManager(models.Manager):                                          
-        def new():                                                              
-                pass                                                            
-        def popular():                                                          
-                pass 
-
+class QuestionManager(models.Manager):
+    def new():
+        pass
+    def popular():
+        pass 
+        
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
