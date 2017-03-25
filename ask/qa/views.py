a2153cd8c73bef4d8e0e277c502a2aee08239e26
иван набойченко < >
2017-03-25 15:48:57 +0300

    added pagination

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -3,7 +3,8 @@
 from django.http import HttpResponse 
 from django.http import Http404 
 from django.shortcuts import render 
-
+from django.core.paginator import Paginator
+from django.core.urlresolvers import reverse
 from models import Question
 
 def test(request, *args, **kwargs):
@@ -11,6 +12,13 @@ def test(request, *args, **kwargs):
 
 def main_questions(request, *args, **kwargs):
     questions = Question.objects.new()
+    limit = request.GET.get('limit',10)
+    page = request.GET.get('page',1)
+    paginator = Paginator(questions,limit)
+    paginator.baseurl = "/?page="
+    page = paginator.page(page)
     return render(request, 'qa/main.html', {
-        'questions': questions
+        'questions': page.object_list,
+        'paginator': paginator,
+        'page': page
     })
