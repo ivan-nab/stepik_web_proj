81e1767e1bf8b80b45928ad52b5f866e3b9ea70c
иван набойченко < >
2017-03-25 17:15:55 +0300

    добавлены обработчики ошибок

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -3,21 +3,34 @@
 from django.http import HttpResponse 
 from django.http import Http404 
 from django.shortcuts import render 
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 from django.core.urlresolvers import reverse
 from models import Question
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK',status=200)
 
+def paginate(request, objects):
+    try:
+        limit = int(request.GET('limit',10))
+    except ValueError:
+        limit = 10
+    if limit > 100 or limit < 0:
+        limit = 10
+    try:
+        page = int(request.GET('page',1))
+    except ValueError:
+        raise Http404
+    paginator = Paginator(objects,limit)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page
+
 def main_questions(request, *args, **kwargs):
     questions = Question.objects.new()
-    limit = request.GET.get('limit',10)
-    page = request.GET.get('page',1)
-    paginator = Paginator(questions,limit)
-    paginator.baseurl = "/?page="
-    questions = paginator.page(page)
+    questions = paginate(request,questions)
     return render(request, 'qa/main.html', {
         'questions': questions
-
     })
