de742e0bf7e11971038090198373eb11aef48b8d
иван набойченко < >
2017-03-31 23:03:11 +0300

    fixed init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo ln -s /home/box/web/etc/gunicorn.django.conf   /etc/gunicorn.d/django
+sudo ln -s web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s web/etc/gunicorn.django.conf   /etc/gunicorn.d/django
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
